856a12f1f369b6ff5a60ac65773804dbc1a46c00
ramtindoc <ramtinhasanzadeh14@gmail.com>
2025-05-25 00:20:23 +0330

    project 1 - complate

---
+++ zeroAndone_stars/zeroAndone_stars.js
@@ -0,0 +1,28 @@
+let zero = ["***", "*.*", "***"];
+let one = [".*.", ".*.", ".*."];
+
+// Equivalents
+let zeroAndone = {
+  0: zero,
+  1: one,
+};
+
+// sample
+let input = "1010";
+
+let output = ["", "", ""];
+
+if (input.length > 100) return;
+
+for (let n = 0; n < input.length; n++) {
+  let digit = input[n]; // get character
+  let lines = zeroAndone[digit]; // Equivalent to the zeros and ones
+
+  for (let line = 0; line <= 3; line++) {
+    output[line] += lines[line];
+  }
+}
+// logger
+console.log(output[0]);
+console.log(output[1]);
+console.log(output[2]);
