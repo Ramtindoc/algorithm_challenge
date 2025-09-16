7be62fc2dd46f12b9771821b7f364c5cbc10ccca
Ramtin Hasanzadeh <148252584+Ramtindoc@users.noreply.github.com>
2025-09-16 23:41:56 +0330

    find_multiples

---
+++ find_multiples/find_multiples.js
@@ -0,0 +1,12 @@
+function findMultiples() {
+  let sum = 0;
+  for (let i = 1; i < 1000; i++) {
+    if ((i % 3 === 0) | (i % 5 === 0)) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(findMultiples());
